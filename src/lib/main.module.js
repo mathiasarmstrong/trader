4b71105b5a1386924c035b5157762144eae9de01
Jered McCullough <jered.mccullough@gmail.com>
2017-06-26 20:34:40 -0700

    changes

--- src/lib/main.module.js
+++
@@ -1,8 +0,0 @@
-m.request({
-  method: 'PUT',
-  url: '/api/v1/users/:id',
-  data: {id: 1, name: 'test'}
-})
-  .then(function(result) {
-    console.log(result);
-  });
--- src/tests.js
+++
@@ -1,7 +0,0 @@
-/// #if !preventAngularEnv
-import './lib/main.extensions';
-import './lib/{**,!sandbox}/*.{module,!messages}.js';
-import 'angular-mocks';
-/// #endif
-
-import 'TESTPATH';
